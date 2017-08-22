47fe302fb7e39ded2254ddd600d78bde9502bf35
marcfolks <marcfolks@gmail.com>
2017-08-22 17:59:45 -0400

    picresizing

--- pizza_app.rb
+++ pizza_app.rb
@@ -3,3 +3,12 @@ require "sinatra"
 get '/' do
     erb :pizza_toppings
 end
+
+post '/comfirmation' do
+    topping = params[:topping]
+    erb :comfirmation, :locals => {:topping=>topping}
+    end
+
+post '/checkout' do
+    final_topping = params[:final_topping]
+    erb :checkout, :locals =>{:}
