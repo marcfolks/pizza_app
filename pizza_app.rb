7e138d4167f178759afc7b3c31df37f80207c0f2
marcfolks <marcfolks@gmail.com>
2017-08-28 19:23:17 -0400

    tying_get_session_to_work

--- pizza_app.rb
+++ pizza_app.rb
@@ -5,10 +5,29 @@ get '/' do
 end
 
 post '/comfirmation' do
-    topping = params[:topping]
-    erb :comfirmation, :locals => {:topping=>topping}
+    session[:meat] = params[:meat]
+    session[:cheese] = params[:cheese]
+    session[:sauce] = params[:sauce]
+    session[:crust] = params[:crust]
+    session[:topping]= params[:topping]
+   redirect '/comfirmation?meat='+ '&meat='+ cheese + '&session=' + sauce + '&sauce=' + crust + '&crust='
+   end
+
+get '/comfirmation' do
+    session[:meat] = params[:meat]
+    session[:cheese] = params[:cheese]
+    session[:sauce] = params[:sauce]
+    session[:crust] = params[:crust]
+erb :comfirmation, :locals => {session[:meat] => meat,:cheese => cheese,:sauce => sauce,:crust => crust,}
     end
 
 post '/checkout' do
-    final_topping = params[:final_topping]
-    erb :checkout, :locals =>{:}
+   final_meat = params[:final_meat]
+   final_cheese = params[:final_cheese]
+   final_sauce = params[:final_sauce]
+   final_crust = params[:final_crust]
+redirect '/checkout?final_meat'+ '&final_meat='+ final_cheese + '&final_cheese=' + final_sauce + '&final_sauce=' + final_crust + '&final_crust=' 
+end
+get '/checkout' do
+   erb :checkout, :locals =>{:final_meat=>final_meat,:final_cheese=>final_cheese,:final_sauce=>final_sauce,:final_crust=>final_crust}
+   end
---
+++ topping_pricing.rb
@@ -0,0 +1,8 @@
+def pizza(topping)
+    pizza = {meat:0,cheese:0,sauce:0,crust:0}
+    topping_value = {meat:1.00,cheese:1.00,sauce:1.00,crust:0.50}
+topping_value.each do |topping,value|
+    end
+
+
+
