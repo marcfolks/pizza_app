8abbfd81d6e7db243ad73837aedff3d91d026812
marcfolks <marcfolks@gmail.com>
2017-08-29 14:24:39 -0400

    workingpictures

--- pizza_app.rb
+++ pizza_app.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+enable :sessions
 
 get '/' do
     erb :pizza_toppings
@@ -9,25 +10,18 @@ post '/comfirmation' do
     session[:cheese] = params[:cheese]
     session[:sauce] = params[:sauce]
     session[:crust] = params[:crust]
-    session[:topping]= params[:topping]
-   redirect '/comfirmation?meat='+ '&meat='+ cheese + '&session=' + sauce + '&sauce=' + crust + '&crust='
-   end
+    redirect '/comfirmation'
+    end
 
 get '/comfirmation' do
-    session[:meat] = params[:meat]
-    session[:cheese] = params[:cheese]
-    session[:sauce] = params[:sauce]
-    session[:crust] = params[:crust]
-erb :comfirmation, :locals => {session[:meat] => meat,:cheese => cheese,:sauce => sauce,:crust => crust,}
+erb :comfirmation, locals:{meat:session[:meat],cheese:session[:cheese],sauce:session[:sauce],crust:session[:crust]}
     end
 
 post '/checkout' do
-   final_meat = params[:final_meat]
-   final_cheese = params[:final_cheese]
-   final_sauce = params[:final_sauce]
-   final_crust = params[:final_crust]
-redirect '/checkout?final_meat'+ '&final_meat='+ final_cheese + '&final_cheese=' + final_sauce + '&final_sauce=' + final_crust + '&final_crust=' 
+   session[:final_topping] = params[:final_topping]
+redirect '/checkout'
 end
+
 get '/checkout' do
-   erb :checkout, :locals =>{:final_meat=>final_meat,:final_cheese=>final_cheese,:final_sauce=>final_sauce,:final_crust=>final_crust}
+   erb :checkout, local:{final_topping:session[:final_topping]}
    end
--- topping_pricing.rb
+++ topping_pricing.rb
@@ -1,8 +1,12 @@
 def pizza(topping)
-    pizza = {meat:0,cheese:0,sauce:0,crust:0}
-    topping_value = {meat:1.00,cheese:1.00,sauce:1.00,crust:0.50}
-topping_value.each do |topping,value|
+    pizza[]
+    topping = 0 + {meat:0,cheese:0,sauce:0,crust:0}
+    topping_value = 5.00 + {meat:1.00,cheese:1.00,sauce:1.00,crust:0.50}
+topping_value.count do |topping,value|
+    value + topping >> pizza
     end
+end
+
 
 
 
